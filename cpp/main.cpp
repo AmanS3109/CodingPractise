df26630c56fcee570f7d8ef0336262ae6f8a175c
AmanS3109 <singhaman2122@ternaengg.ac.in>
2024-07-24 17:37:33 +0530

    commited message

---
+++ cpp/main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout << 'c' + 1 << endl; // ascii of c + 1 = 99 + 1
+    //conditions
+    int a = 2;
+    int b = 3;
+    int c = 4;
+    cout << (a != b) << endl;
+    cout << ((b > a) || (b > c));
+    return 0;
+}
